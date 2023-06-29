413df1800b2f8d8782b2d2f8269834cab6b2fd92
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-06-30 00:31:07 +0300

    user model created

--- routers/categories.js
+++ routers/categories.js
@@ -29,7 +29,7 @@ router.post('/', async(req,res)=>{
     category = await category.save()
 
     if(!category)
-    return res.status(404).send('the category cannot be created.')
+    return res.status(400).send('the category cannot be created.')
 
     res.status(201).json({message: 'Category created successfully', category})
 })
