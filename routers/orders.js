0d9bf6d857988ec514712ee1071d7418f33e2128
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-07-10 12:21:42 +0300

    orders and order items created

--- routers/orders.js
+++ routers/orders.js
@@ -1,14 +1,49 @@
 const {Order} = require('../models/order')
 const express = require('express')
+const { OrderItem } = require('../models/order-item');
 const router = express.Router()
 
 router.get(`/`, async(req,res)=>{
-    const userList = await Order.find()
+    const orderList = await Order.find()
 
     if(!orderList){
         res.status(500).json({succes: false})
     }
     res.send(orderList)
 })
+
+router.post('/', async(req,res)=>{
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem)=>{
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save()
+        return newOrderItem._id
+    }))
+    const orderItemsIdsResolved =  await orderItemsIds;
+    // console.log(orderItemsIdsResolved)
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        //totalPrice: req.body.totalPrice,
+        user: req.body.user,   //girilen token ile id si aynı olan kullanıcı olmalı.
+    })
+    order = await order.save();
+
+    if(!order)
+    return res.status(400).send('the order cannot be created.')
+
+    res.status(200).json({message: 'order created successfully', order})
+})
+
+
 
 module.exports = router
